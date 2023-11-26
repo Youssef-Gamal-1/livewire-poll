aef670c06b4f477b5cd4b39cbc54226e5dc0be79
Youssef <yg9552753@gmail.com>
2023-11-26 11:33:10 +0200

    Livewire actions

--- app/Livewire/CreatePoll.php
+++ app/Livewire/CreatePoll.php
@@ -7,9 +7,16 @@ use Livewire\Component;
 class CreatePoll extends Component
 {
     public $title;
-
+    public array $options = ['First'];
     public function render()
     {
         return view('livewire.create-poll');
     }
+    public function addOption(): void{
+        $this->options[] = '';
+    }
+//    public function mount(){
+//      for initializing properties if you have a lot of logic to put
+//      it will called once
+//    }
 }
--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -4,5 +4,15 @@
         <input type="text" wire:model.live="title">
 {{--wire:model.live.depounce.0ms='title'--}}
         Current Title: {{ $title }}
+        <div class="mb-4 mt-4">
+            <button class="btn" wire:click.prevent="addOption">Add option</button>
+        </div>
+
+        @foreach($options as $index => $option)
+            <div class="mb-4">
+                {{$index}} - {{$option}}
+            </div>
+        @endforeach
+
     </form>
 </div>
